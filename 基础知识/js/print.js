05b3a5867d93bf887d84f04f7700807c32cb6266
wangsn <123@qq.com>
2024-09-17 22:46:45 +0800

    basic01

---
+++ 基础知识/js/print.js
@@ -0,0 +1,10 @@
+// var num = 12
+// var str = prompt(num)
+// alert(str)
+// console.log('dfsdf');
+// console.dir({name:'李四',age:22});
+// console.log({name:'李四',age:22});
+console.table({name:'王鹏',age:30,sex:'male'}); 
+
+
+
